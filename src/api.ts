a32b1d20f6ce276863305402abf57bdce07a0fe3
ilgon0110 <moses011010@gmail.com>
2023-02-13 16:27:09 +0900

    feat : API 기능 추가
    요구사항에 맞는 config header 추가
    signup, signin api 생성

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,16 @@ import { ISignUpForm } from "./views/SignUp";
 const api = axios.create({
   baseURL: "https://pre-onboarding-selection-task.shop/",
 });
+
 export const signApi = {
-  signUp: (payload: ISignUpForm) => api.post("auth/signup", payload),
+  signUp: (payload: ISignUpForm) => {
+    return api.post("auth/signup", payload, {
+      headers: { "Content-Type": "application/json" },
+    });
+  },
+  signIn: (payload: ISignUpForm) => {
+    return api.post("auth/signin", payload, {
+      headers: { "Content-Type": "application/json" },
+    });
+  },
 };
